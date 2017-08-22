2c06ef998d8ca4b79c49a8ac8ed1f4b6fbefae1b
lymanjohnson <lyman.johnson@gmail.com>
2017-08-22 16:52:37 -0400

    Finished

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "mustache");
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   fs.readFile('data.json','utf8', function readFileCallback(err,data){
@@ -22,59 +22,62 @@ app.get("/", function (req, res) {
     console.log(err);
   }
   else {
-    let todoArray = JSON.parse(data);
+    let todoFileContents = JSON.parse(data);
+    let todoArray = todoFileContents.list;
     // let todoArray = obj.todos;
+    console.log("Got!");
     console.log(todoArray);
-    res.render('index', {todoArray : todoArray});
+    res.render('index', {todoMustache : todoArray});
   }
 })});
 
-
-/*fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
-    if (err){
-        console.log(err);
-    } else {
-    obj = JSON.parse(data);//Turns data.json into an object named obj
-    var todoarray = obj.todoArray; //declares makes the todoarray
-    var donearray = obj.doneArray; //declares makes the donearray
-    // this makes todosMustache the todoarray var
-    res.render('index', { todosMustache: todoarray,  doneMustache: donearray});
-*/
-
 app.post("/", function(req,res) {
   let newEntry = req.body.newEntry
+  console.log("Post!");
+  console.log("newEntry",newEntry);
   fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
     if (err){
       console.log(err);
     }
     else {
-      let listObject = JSON.parse(data); //now its an object
-      let newEntryObject = "{'item':'"+newEntry+"','category':'Home','status':'incomplete'}";
-      listObject.push(newEntryObject); //pushes the text to an array
-      let json = JSON.stringify(obj); //converts back to json
+      let todoFileContents = JSON.parse(data);
+      console.log("todoFileContents",todoFileContents);
+      let newEntryObject = {};
+      newEntryObject.item = newEntry;
+      newEntryObject.category = "Home";
+      newEntryObject.status = "incomplete";
+      console.log("newEntryObject",newEntryObject);
+      todoFileContents.list.push(newEntryObject); //pushes the text to an array
+      console.log("new array",todoFileContents);
+      let json = JSON.stringify(todoFileContents); //converts back to json
       fs.writeFile('data.json', json, 'utf8'); // writes to file
 }});
-
+  console.log("about to redirect...");
   res.redirect('/');//reloads page
 });
 
-// app.post("/", function(req, res)){
-//   let newEntry = req.body.newEntry
-//
-// }
 
-// app.post("/", function (req, res) {
-//   todoList.push(req.body.todo);
-//   console.log("pushed!");
-//   console.log(toDoList(toDoList.length));
-//   res.redirect('/');
-// });
+app.post("/:dynamic", function (req, res) {
+  let clickedItem = req.params.dynamic;
+  console.log(clickedItem);
 
-// app.post('/', function(req, res){
-//   var email = req.body.email;
-//   var html = '<p>Your user name is: </p>' + email;
-//   res.send(html);
-// });
+  fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+
+      let todoFileContents = JSON.parse(data); //now its an object
+
+      for (i=0 ; i<todoFileContents.list.length ; i++){
+        if (todoFileContents.list[i].item == clickedItem){
+          todoFileContents.list[i].status = "complete";
+        }
+      }
+      let json = JSON.stringify(todoFileContents); //converts back to json
+      fs.writeFile('data.json', json, 'utf8'); // writes to file
+  }});
+  res.redirect('/');//reloads page
+})
 
 
 app.listen(3000, function () {
